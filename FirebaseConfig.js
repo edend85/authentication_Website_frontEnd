cbd49afab2ea87475cdf5ce1191ec4f665d03955
edend85 <117260021+edend85@users.noreply.github.com>
2024-01-18 16:07:32 +0200

    change login

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,21 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth,FacebookAuthProvider } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDYK7H3F4AVG1OuFmsJOFPqP1_kLl0DazI",
-  authDomain: "socialmedialogin-ed0b2.firebaseapp.com",
-  projectId: "socialmedialogin-ed0b2",
-  storageBucket: "socialmedialogin-ed0b2.appspot.com",
-  messagingSenderId: "182123577008",
-  appId: "1:182123577008:web:7c5c152819a0ef6ae1c575",
-  measurementId: "G-5MNXL1YNCJ"
+  apiKey: "AIzaSyAoMtrDWFlW1cc9cpTCDg0_wBhDX2Sqw4E",
+  authDomain: "authenticationwebsite-6901f.firebaseapp.com",
+  projectId: "authenticationwebsite-6901f",
+  storageBucket: "authenticationwebsite-6901f.appspot.com",
+  messagingSenderId: "815653281370",
+  appId: "1:815653281370:web:848fa745d2fa8d5c950263",
+  measurementId: "G-L6Y5CGVRRT"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 const provider = new FacebookAuthProvider();
-
 export {auth,provider}
 
