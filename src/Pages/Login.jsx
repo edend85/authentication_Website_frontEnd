8b267eabb16f802c16920d94583a808c659d2294
edend85 <117260021+edend85@users.noreply.github.com>
2024-01-21 10:50:27 +0200

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -28,8 +28,8 @@ export default function Login() {
     const provider = new FacebookAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
-        /* const u = getAdditionalUserInfo(result);
-         console.log('result.user :>> ', u);*/
+        const u = getAdditionalUserInfo(result);
+        console.log('result.user :>> ', u);
         const user = {
           firstName: u.profile.efirstName,
           lastName: u.profile.lastName,
