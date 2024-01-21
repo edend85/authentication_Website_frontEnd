71bb5aabf4a03815d8ab89d8ad18795de83880df
edend85 <117260021+edend85@users.noreply.github.com>
2024-01-21 12:21:50 +0200

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -33,10 +33,7 @@ export default function Login() {
         const accessToken = credential.accessToken;
         fetch(`https://graph.facebook.com/${result.user.providerData[0].uid}/picture?type=large&access_token=${accessToken}`)
           .then((response) => {
-            response.blob()
-          })
-          .then((blob) => {
-            setImg(URL.createObjectURL(blob))
+            setImg(URL.createObjectURL(response.blob()))
           })
         const user = {
           firstName: u.profile.first_name,
