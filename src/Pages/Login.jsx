03bdc7f1d1edf797c08ee2749cd954d545e78570
edend85 <117260021+edend85@users.noreply.github.com>
2024-01-23 12:45:32 +0200

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -39,6 +39,7 @@ export default function Login() {
         fetch(`https://graph.facebook.com/${result.user.providerData[0].uid}/picture?type=large`)
           .then((response) => {
             console.log('response :>> ', response);
+            console.log('response :>> ', response.blob());
             /*console.log('object :>> ', response.blob());*/
             /*response.blob()*/
             /*setImg(URL.createObjectURL(response.blob()))*/
