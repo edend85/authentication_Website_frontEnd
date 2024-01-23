f24169cf5353b587175963d6358453d4d92de3ca
edend85 <117260021+edend85@users.noreply.github.com>
2024-01-23 16:48:35 +0200

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
       .then((result) => {
         const credential = FacebookAuthProvider.credentialFromResult(result);
         const accessToken = credential.accessToken
-        fetch(`https://graph.facebook.com/${result.user.providerData[0].uid}/fields=name,email,picture&access_token=${accessToken}`, {
+        fetch(`https://graph.facebook.com/${result.user.providerData[0]}/fields=name,email,picture&access_token=${accessToken}`, {
           method: 'GET',
         })
           .then(response => response.blob())
